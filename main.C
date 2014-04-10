0c121fc92751599699e9de7553aa36f640e71489
Daniel Smith <jellymann@gmail.com>
2014-04-10 05:31:26 +0200

    Added checkerboard to floor

--- main.C
+++ main.C
@@ -220,22 +220,34 @@ int main()
 
   const char *vertex_glsl = 
     "#version 430\n"
-    "uniform mat4 view;\n"
-    "uniform mat4 projection;\n"
-    "uniform mat4 world;\n"
-    "layout (location=1) in vec3 position;\n"
-    "void main()\n"
-    "{\n"
-      "gl_Position = projection * view * world * vec4(position,1.0f);\n"
+    "uniform mat4 view;"
+    "uniform mat4 projection;"
+    "uniform mat4 world;"
+    "layout (location=1) in vec3 position;"
+    "out vec3 world_position;"
+    "void main()"
+    "{"
+      "world_position = (world * vec4(position,1.0f)).xyz;"
+      "gl_Position = projection * view * world * vec4(position,1.0f);"
       // "gl_Position = vec4(position,1.0f);\n"
-    "}\n";
+    "}";
   const char *fragment_glsl =
     "#version 430\n"
-    "layout (location = 0) out vec4 colour;\n"
-    "void main()\n"
-    "{\n"
-      "colour = vec4(1,0,1,1);\n"
-    "}\n";
+    "layout (location = 0) out vec4 colour;"
+    "in vec3 world_position;"
+    "void main()"
+    "{"
+      "vec3 wp = world_position + vec3(100,100,100);"
+      "float scale = 0.5;"
+      "float xm = mod(int(wp.x*scale),2);"
+      "float ym = mod(int(wp.y*scale),2);"
+      "float zm = mod(int(wp.z*scale),2);"
+      "float ip = 0;"
+      "if ("
+        "xm == zm"
+        ") ip = 0.5;"
+      "colour = vec4(ip,ip,ip,1);"
+    "}";
 
   GLFWwindow* window;
 
