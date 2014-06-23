54a7b6b7c05b6f59989b791bdd8f27ea11a946d9
Daniel Smith <jellymann@gmail.com>
2014-06-23 22:19:10 +0200

    Fixed misleading 5th column in performance.csv

--- calculate_performance.rb
+++ calculate_performance.rb
@@ -2,7 +2,7 @@ lines = ARGF.each_line.to_a
 
 times = lines[-1].chomp.split(',')
 
-ms_per_frame = times[times.size/2..-1].map do |x|
+ms_per_frame = times[times.size/2..-2].map do |x|
   (x.to_f / lines.size.to_f) * 1000
 end
 
